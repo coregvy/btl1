b900fed14f81174437ddc4ba6a3e726de3100a24
yamada <coregvy@gmail.com>
2017-01-15 13:13:44 +0900

    init commit.

---
+++ Assets/Scripts/BgManager.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BgManager : MonoBehaviour
+{
+    [SerializeField]
+    GameObject playerSprite;
+
+    // Use this for initialization
+    void Start()
+    {
+        Debug.Log("start: " + playerSprite.GetType());
+        Sprite[] tiles = Resources.LoadAll<Sprite>("baseTiles");
+        float tileSize = 0.16f;
+        float tileScale = 20.0f;
+        var basePose = new Vector2(0, 0);
+        var tileBasePose = new Vector2(-6, 6);
+        const int tileX = 5;
+        const int tileY = 4;
+        // transform.position = new Vector3(basePose.x + tileSize * tileScale * 2.0f, basePose.y + tileSize * tileScale * 1.5f);
+        int[][] tileMap = {
+            new int[]{ 0, 1, 3, 3, 5 },
+            new int[]{ 24, 27, 99, 99, 77 },
+            new int[]{ 24, 27, 99, 99, 77 },
+            new int[]{120, 121, 122, 122, 125}
+        };
+        for (int y = 0; y < tileMap.Length; ++y)
+        {
+            for (int x = 0; x < tileMap[y].Length; ++x)
+            {
+                var tile = new GameObject("Sprite" + x + "-" + y);
+                var tileRenderer = tile.AddComponent<SpriteRenderer>();
+                tileRenderer.sprite = tiles[tileMap[y][x]];
+                tileRenderer.transform.position = new Vector3(tileBasePose.x + tileSize * tileScale * x, tileBasePose.y - tileSize * tileScale * y, 10.0f);
+                tileRenderer.transform.localScale = new Vector3(tileScale, tileScale);
+                tileRenderer.transform.parent = transform;
+                tile.AddComponent<BgTile>().SetParent(this);
+            }
+        }
+    }
+
+    void Update()
+    {
+
+    }
+
+    public bool isPlayerOn(BgTile tile)
+    {
+        if(tile.transform.position == playerSprite.transform.position)
+        {
+            return true;
+        }
+        return false;
+    }
+}
---
+++ Assets/Scripts/BgTile.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(BoxCollider2D))]
+public class BgTile : MonoBehaviour {
+
+    BgManager bgparent;
+    
+    public void SetParent(BgManager bgparent)
+    {
+        this.bgparent = bgparent;
+    }
+    // Use this for initialization
+    void Start()
+    {
+        GetComponent<BoxCollider2D> ().size = new Vector2 (0.16f, 0.16f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    void OnMouseEnter()
+    {
+        Debug.Log("mouse enter: " + name);
+        GetComponent<SpriteRenderer>().color = new Color(0.5f, 0.5f, 0.5f);
+    }
+    void OnMouseExit()
+    {
+        Debug.Log("mouse exit: " + name);
+        GetComponent<SpriteRenderer>().color = new Color(1.0f, 1.0f, 1.0f);
+    }
+
+    private Vector3 screenPoint;
+    private Vector3 offset;
+    void OnMouseDown()
+    {
+        Debug.Log("bg mouse down: " + name);
+        this.screenPoint = Camera.main.WorldToScreenPoint(transform.parent.transform.position);
+        this.offset = transform.parent.transform.position - Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z));
+    }
+
+    void OnMouseDrag()
+    {
+        Vector3 currentScreenPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z);
+        Vector3 currentPosition = Camera.main.ScreenToWorldPoint(currentScreenPoint) + this.offset;
+        transform.parent.transform.position = currentPosition;
+    }
+    void OnMouseUp()
+    {
+        if(bgparent.isPlayerOn(this))
+        {
+            Debug.Log("player is none.");
+        }
+        else
+        {
+            Debug.Log("player on this tile.");
+        }
+    }
+}
