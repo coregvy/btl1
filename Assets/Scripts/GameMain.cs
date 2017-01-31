cf3851ded02d0cb170eeccc8c7ef7884036d5bcb
yamada-tk <yamada-tk@njk.co.jp>
2017-01-31 20:41:56 +0900

    add.

---
+++ Assets/Scripts/GameMain.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameMain : SingletonMonoBehaviourFast<GameMain> {
+    Scenes nowScene = Scenes.Title;
+    [SerializeField]
+    GameConfig config;
+	// static GameMain singleton = null;
+
+	// Use this for initialization
+	void Start () {
+		config = new GameConfig ();
+		DontDestroyOnLoad (this);
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void changeScene(Scenes nextScene) {
+		nowScene = nextScene;
+		SceneManager.LoadScene (nextScene.SceneName());
+	}
+}
---
+++ Assets/Scripts/Scenes.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+public enum Scenes
+{
+    Title,
+    Config,
+}
+
+
+public static class ScenesExt {
+    public static string SceneName(this Scenes scene) {
+        return scene.ToString ();
+    }
+}
