b68f2d0c108fb6f000a0edaadff595f14f75d7fa
yamada-tk <yamada-tk@njk.co.jp>
2017-02-02 19:37:25 +0900

    update.

--- Assets/Scripts/GameMain.cs
+++ Assets/Scripts/GameMain.cs
@@ -3,25 +3,29 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class GameMain : SingletonMonoBehaviourFast<GameMain> {
+public sealed class GameMain {
+	private static GameMain instance = new GameMain();
+
+	public static GameMain Instance {
+		get {
+			return instance;
+		}
+	}
+	private GameMain(){
+	}
+
     Scenes nowScene = Scenes.Title;
-    [SerializeField]
-    GameConfig config;
-	// static GameMain singleton = null;
 
-	// Use this for initialization
-	void Start () {
-		config = new GameConfig ();
-		DontDestroyOnLoad (this);
-	}
-
-	// Update is called once per frame
-	void Update () {
-		
-	}
-
-	void changeScene(Scenes nextScene) {
+	public void changeScene(Scenes nextScene) {
 		nowScene = nextScene;
 		SceneManager.LoadScene (nextScene.SceneName());
+	}
+	int count = 0;
+	public int incrementCounter() {
+		return count++;
+	}
+
+	public GameConfig getConfig(){
+		return null;
 	}
 }
---
+++ Assets/Scripts/SceneChanger.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneChanger : MonoBehaviour {
+
+	[SerializeField]
+	Scenes nextScene;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	public void ChangeScene(){
+		Debug.Log ("change scene -> " + nextScene.SceneName());
+		Debug.Log ("counter: " + GameMain.Instance.incrementCounter ());
+		GameMain.Instance.changeScene (nextScene);
+	}
+}
